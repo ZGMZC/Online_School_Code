083387b3c3a333bd6dbf24d864230463106d6277
ZGMZC <zhuguangmao1002@foxmail.com>
2022-10-02 15:40:08 +0800

    Initial commit

---
+++ src/main/java/com/school/online_school_code/OnlineSchoolCodeApplication.java
@@ -0,0 +1,13 @@
+package com.school.online_school_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSchoolCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/online_school_code/OnlineSchoolCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.online_school_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
